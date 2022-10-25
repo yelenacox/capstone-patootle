7688884db648898f46868eee41a7221ad854d08c
Yelena Cox <yelenacox@gmail.com>
2022-10-25 11:25:04 -0500

    added log in and register forms

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import { Route, Routes } from "react-router-dom"
-// import { Authorized } from "./views/Authorized"
-// import { ApplicationViews } from "./views/ApplicationViews"
-// import { NavBar } from "./nav/NavBar"
-
 import "./App.css"
 import { Login } from "./components/auth/Login"
 import { Register } from "./components/auth/Register"
+import { RegisterBaby } from "./components/auth/RegisterBaby"
+import { NavBar } from "./components/nav/NavBar"
+import { Authorized } from "./components/views/Authorized"
+// import { ApplicationViews } from "./views/ApplicationViews"
 
 
 export const App = () => {
 	return <Routes>
 		<Route path="/login" element={<Login />} />
 		<Route path="/register" element={<Register />} />
+		<Route path="/add_baby" element={<RegisterBaby />} />
 
-		{/* <Route path="*" element={
+
+		<Route path="*" element={
 			<Authorized>
 				<>
 					<NavBar />
-					<ApplicationViews />
+					{/* <ApplicationViews /> */}
 				</>
 			</Authorized>
 
-		} /> */}
+		} />
 	</Routes>
 }
---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,28 @@
+import { Link, useNavigate } from "react-router-dom"
+import "./NavBar.css"
+
+export const NavBar = () => {
+    const navigate = useNavigate()
+
+    return (
+        <ul className="navbar">
+            <li className="navbar__item active">
+                <Link className="navbar__link" to="/create">Add New Entry</Link>
+            </li>
+            <li className="navbar__item active">
+                <Link className="navbar__link" to="/all">All Entries</Link>
+            </li>
+            <li className="navbar__item active navbar__right">
+                <Link className="navbar__link" to="/profile">Profile</Link>
+            </li>
+            {
+                <li className="navbar__item navbar__right">
+                    <Link className="navbar__link" to="" onClick={() => {
+                        localStorage.removeItem("app_user")
+                        navigate("/", { replace: true })
+                    }}>Logout</Link>
+                </li>
+            }
+        </ul>
+    )
+}
