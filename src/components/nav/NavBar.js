1869c12fd5e004c12f4018fb563ab4b6bc935fc2
Yelena Cox <yelenacox@gmail.com>
2022-11-13 23:29:24 -0600

    made pictures show up on dropdown and entry type

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,15 +1,16 @@
 import { Link, useNavigate } from "react-router-dom"
 import { NavBabyChoice } from "./NavBabyChoice"
 import "./NavBar.css"
+import PatootleLogoTransparent from '../../images/Patootle-Logo-transparent.png'
+import { useEffect } from "react"
 
 
 export const NavBar = ({userBabies, setUserBabies, selectedUserBaby, setSelectedUserBaby}) => {
     const navigate = useNavigate()
-
     return (
         <ul className="navbar">
             <div className="navbar__item__center">
-                <li className="navbar__item active"><Link to="/"><img className="nav_logo" src="https://i.ibb.co/gW6PVmb/Patootle-Logo-transparent.png" /></Link></li>
+                <li className="navbar__item active"><Link to="/"><img className="nav_logo" src={PatootleLogoTransparent} /></Link></li>
                 <li className="navbar__item active navbar__create navbar__center">
                     <Link className="navbar__link" to="/create">new entry</Link>
                 </li>
